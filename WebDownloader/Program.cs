c28dd867312b64faa75aea731d8c8ed0e8f48686
Simen Andre Skogum <v0nNemizez@gmail.com>
2018-05-22 11:32:15 +0200

    Add project files.

---
+++ WebDownloader/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Net.Http;
+
+namespace WebDownloader
+{
+     class Program
+    {
+         static async void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Starting HTTP Client");
+            HttpClient client = new HttpClient(); 
+
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync("http://google.com");
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+
+                Console.WriteLine(responseBody);
+            }
+            catch(HttpRequestException e)
+            {
+                Console.WriteLine("\nException Caught!");
+                Console.WriteLine("Message :{0} ", e.Message);
+                Console.WriteLine("Press a key to Exit");
+                Console.ReadKey();
+
+            }
+
+            client.Dispose();
+        }
+
+
+
+    }
+      
+}
